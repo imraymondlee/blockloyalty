dede6c7364c6ce63d07d279d8f0eb53614caf834
imraymondlee <imleeraymond@gmail.com>
2021-06-26 15:26:48 -0400

    Add address when adding new customer

--- src/contracts/LoyaltyCard.sol
+++ src/contracts/LoyaltyCard.sol
@@ -5,7 +5,7 @@ contract LoyaltyCard {
 
     struct Customer {
         uint256 id;
-        // address account;
+        address account;
         uint256 balance;
     }
 
@@ -13,14 +13,22 @@ contract LoyaltyCard {
 
     constructor() public {
         owner = msg.sender;
-        addCustomer(4, 10);
+        addCustomer(4, 0xe04eF06a5652E224Ba2E63507bbf474d396967d4, 10);
     }
 
-    event CustomerAdded(uint256 id, uint256 balance);
+    event CustomerAdded(uint256 id, address account, uint256 balance);
 
-    function addCustomer(uint256 id, uint256 balance) public {
-        Customer memory newCustomer = Customer({id: id, balance: balance});
+    function addCustomer(
+        uint256 id,
+        address account,
+        uint256 balance
+    ) public {
+        Customer memory newCustomer = Customer({
+            id: id,
+            account: account,
+            balance: balance
+        });
         customers[id] = newCustomer;
-        emit CustomerAdded(id, balance);
+        emit CustomerAdded(id, account, balance);
     }
 }
