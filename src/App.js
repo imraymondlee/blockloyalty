34bbb60a82aebaf7a9003f8f5b641129aa04f748
imraymondlee <imleeraymond@gmail.com>
2021-07-01 01:45:10 -0400

    Style main page

--- src/App.js
+++ src/App.js
@@ -63,20 +63,26 @@ const App = () => {
   };
 
   return (
-    <Box textAlign="center" m={4}>
-      <Stack spacing={3}>
-        <Heading as="h2" size="md" color="#5611b2" my={3}>
+    <React.Fragment>
+      <Box textAlign="center" bg="#6100e7" height="250px" p={6}>
+        <Heading as="h2" size="md" color="white" mb={3}>
           BlockLoyalty
         </Heading>
-        <Skeleton height="100px" />
-        <Text fontSize="xs" fontWeight={700}>
-          {account}
-        </Text>
-        <Text fontSize="md">Owner: {isOwner ? 'yes' : 'no'}</Text>
+      </Box>
+      <Box textAlign="center" mx={4} mt="-160px">
+        <Stack spacing={3}>
+          <Box bg="white" p={4} rounded="lg" boxShadow="lg">
+            <Skeleton height="200px" />
+            <Text fontSize="10px" fontWeight={700} my={2}>
+              {account}
+            </Text>
+          </Box>
+          <Text fontSize="md">Owner: {isOwner ? 'yes' : 'no'}</Text>
 
-        <StampCard balance={balance} />
-      </Stack>
-    </Box>
+          <StampCard balance={balance} />
+        </Stack>
+      </Box>
+    </React.Fragment>
   );
 };
 
